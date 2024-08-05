574ebd161da3e5ba106939cd6bf13f86c9d47276
pradeepleodas <wwwpradeeprolex20@gmail.com>
2024-08-04 15:51:29 -1200

    Add files via upload

---
+++ pradeep.py
@@ -0,0 +1,8 @@
+a=int(input("the age of ram"))
+b=int(input("the age of sham"))
+c=int(input("the age of ajay"))
+if a>b and a>c:
+    print("ram is youngest of 3")
+elif b>a and b>c:
+    print("sham is the youngest of 3")
+else:print("ajay is youngest of 3")
